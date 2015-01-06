a4169d24e897b2134ae80e99424f7b565bed157e
Martin Gerhardy <martin.gerhardy@gmail.com>
2015-01-06 08:45:03 +0100

    * reorganized some stuff

--- src/voxel/src/TestBlockLandApplication.cpp
+++ src/voxel/src/TestBlockLandApplication.cpp
@@ -3,34 +3,34 @@
 #include <OIS/OIS.h>
 #include <iostream>
 
-blockinfo_t g_BlockInfo[] = { { 0, "Air", Ogre::ColourValue(1.0, 1.0, 1.0) }, { 1, "Grass", Ogre::ColourValue(0.0, 0.5, 0.0) }, { 2, "Soil", Ogre::ColourValue(0.5, 0.5, 0.0) }, {
-		3, "Rock", Ogre::ColourValue(0.5, 0.5, 0.5) }, { 4, "Lava", Ogre::ColourValue(0.8, 0.0, 0.0) }, { 255, "", Ogre::ColourValue(0.0, 0.5, 0.0) } };
+const BlockInfo BLOCKINFO[] = {
+	{ BlockType::Air, "Air", Ogre::ColourValue(1.0, 1.0, 1.0) },
+	{ BlockType::Grass, "Grass", Ogre::ColourValue(0.0, 0.5, 0.0) },
+	{ BlockType::Soil, "Soil", Ogre::ColourValue(0.5, 0.5, 0.0) },
+	{ BlockType::Rock, "Rock", Ogre::ColourValue(0.5, 0.5, 0.5) }
+};
 
-struct layer_t {
-	block_t BlockID;
+struct Layer {
+	BlockType BlockID;
 	int MinLevel;
 	int MaxLevel;
 	int SeedOffset;
 };
 
-layer_t g_Layers[] = { { 1, 0, 2, 1 },		//Grass
-		{ 2, 0, 10, 2 },	//Soil
-		{ 3, 20, 200, 3 },	//Rock
-		{ 4, 100, 300, 4 },     //Lava
-		{ 255, 0, 0, 0 }        //Must be last
+const Layer LAYERS[] = {
+	{ BlockType::Grass, 0, 2, 1 },
+	{ BlockType::Soil, 0, 10, 2 },
+	{ BlockType::Rock, 20, 200, 3 }
 };
 
-TestBlockLandApplication::TestBlockLandApplication() {
-	m_ChunkID = 1;
-	m_LightAngle = 90;
-	m_LightColor = Ogre::ColourValue(1, 1, 1);
-	m_FogColor = m_LightColor * 0.8f;
-	m_AmbientColor = m_LightColor / 3.0f;
+TestBlockLandApplication::TestBlockLandApplication() :
+		_worldXSize(-1), _worldYSize(-1), _chunkID(1), _blocks(nullptr), _lightAngle(90), _worldTime(0.0f), _updateChunkX(0), _updateChunkY(0), _updateChunkZ(0), _updateChunksCount(
+				0), _blockChunkObjects(nullptr), _lightColor(Ogre::ColourValue(1, 1, 1)), _fogColor(_lightColor * 0.8f), _ambientColor(_lightColor / 3.0f), _blockVertexCount(
+				nullptr) {
 }
 
 TestBlockLandApplication::~TestBlockLandApplication() {
-	delete[] m_Blocks;
-	delete[] m_BlockLight;
+	delete[] _blocks;
 }
 
 #if 0
@@ -47,9 +47,9 @@ void TestBlockLandApplication::initWorldBlocksCaves() {
 	NoiseSource.SetSeed(0);
 	NoiseSource.SetOctaveCount(4);
 
-	for (y = 0, ny = 0; y < WORLD_SIZE; ++y, ny += delta) {
-		for (z = 0, nz = 0; z < WORLD_SIZE; ++z, nz += delta) {
-			for (x = 0, nx = 0; x < WORLD_SIZE; ++x, nx += delta) {
+	for (y = 0, ny = 0; y < _worldHeight; ++y, ny += delta) {
+		for (z = 0, nz = 0; z < _worldHeight; ++z, nz += delta) {
+			for (x = 0, nx = 0; x < _worldHeight; ++x, nx += delta) {
 				float Value = NoiseSource.GetValue(nx, ny, nz);
 				if (Value > ValueLimit)
 				GetBlock(x, y, z) = 0;
@@ -59,29 +59,29 @@ void TestBlockLandApplication::initWorldBlocksCaves() {
 }
 
 void TestBlockLandApplication::initWorldBlocksLayers() {
-	int HeightMap[WORLD_SIZE][WORLD_SIZE];
+	int HeightMap[_worldHeight][_worldHeight];
 	infland::CLandscape LayerMaps[10];  //Hard coded because I'm lazy!
 	int NumLayerMaps = 0;
 	int BaseSeed;
 
-	m_Landscape.SetSize(WORLD_SIZE, WORLD_SIZE);
+	m_Landscape.SetSize(_worldHeight, _worldHeight);
 	m_Landscape.CreateAltitude();
 	BaseSeed = m_Landscape.GetSeed();
 
 	infland::CMap& Map = m_Landscape.GetAltitudeMap();
 
 	//Initialize our temporary height map array
-	for (int z = 0; z < WORLD_SIZE; ++z) {
-		for (int x = 0; x < WORLD_SIZE; ++x) {
-			float Height = Map.GetValue(x, z) * WORLD_SIZE / 4 + WORLD_SIZE / 2;
+	for (int z = 0; z < _worldHeight; ++z) {
+		for (int x = 0; x < _worldHeight; ++x) {
+			float Height = Map.GetValue(x, z) * _worldHeight / 4 + _worldHeight / 2;
 			HeightMap[x][z] = Height;
 		}
 	}
 
 	//Create height maps for each layer
-	for (int i = 0; g_Layers[i].BlockID != BLOCK_NULL && i < 10; ++i) {
-		LayerMaps[i].SetSeed(BaseSeed + g_Layers[i].SeedOffset);
-		LayerMaps[i].SetSize(WORLD_SIZE, WORLD_SIZE);
+	for (int i = 0; LAYERS[i].BlockID != BLOCK_NULL && i < 10; ++i) {
+		LayerMaps[i].SetSeed(BaseSeed + LAYERS[i].SeedOffset);
+		LayerMaps[i].SetSize(_worldHeight, _worldHeight);
 		LayerMaps[i].CreateAltitude();
 		++NumLayerMaps;
 	}
@@ -92,11 +92,11 @@ void TestBlockLandApplication::initWorldBlocksLayers() {
 	for (int layer = 0; layer < NumLayerMaps; ++layer) {
 		infland::CMap & Map = LayerMaps[layer].GetAltitudeMap();
 
-		for (int z = 0; z < WORLD_SIZE; ++z) {
-			for (int x = 0; x < WORLD_SIZE; ++x) {
+		for (int z = 0; z < _worldHeight; ++z) {
+			for (int x = 0; x < _worldHeight; ++x) {
 				if (HeightMap[x][z] <= 0)
 				continue;
-				int Height = (Map.GetValue(x, z) + 1) / 2.0f * (g_Layers[layer].MaxLevel - g_Layers[layer].MinLevel) + g_Layers[layer].MinLevel;
+				int Height = (Map.GetValue(x, z) + 1) / 2.0f * (LAYERS[layer].MaxLevel - LAYERS[layer].MinLevel) + LAYERS[layer].MinLevel;
 
 				//Don't fill the map below 0 height
 				if (HeightMap[x][z] - Height < 0)
@@ -106,7 +106,7 @@ void TestBlockLandApplication::initWorldBlocksLayers() {
 				int MaxHeight = HeightMap[x][z] + Height;
 
 				for (int y = HeightMap[x][z]; y <= MaxHeight; ++y) {
-					GetBlock(x, y, z) = g_Layers[layer].BlockID;
+					GetBlock(x, y, z) = LAYERS[layer].BlockID;
 				}
 			}
 		}
@@ -116,17 +116,17 @@ void TestBlockLandApplication::initWorldBlocksLayers() {
 
 void TestBlockLandApplication::initWorldBlocksLight() {
 	int x, y, z;
-	blocklight_t Light;
-	blocklight_t DeltaLight = 16;
+	LightValue Light;
+	LightValue DeltaLight = 16;
 
-	for (z = 0; z < WORLD_SIZE; ++z) {
-		for (x = 0; x < WORLD_SIZE; ++x) {
+	for (z = 0; z < _worldHeight; ++z) {
+		for (x = 0; x < _worldHeight; ++x) {
 			Light = 255;
 
-			for (y = WORLD_SIZE - 1; y >= 0; --y) {
-				GetBlockLight(x, y, z) = Light;
+			for (y = _worldHeight - 1; y >= 0; --y) {
+				getBlockLight(x, y, z) = Light;
 
-				if (GetBlock(x, y, z) != 0) {
+				if (getBlock(x, y, z) != 0) {
 					if (Light >= DeltaLight)
 						Light -= DeltaLight;
 					else
@@ -138,22 +138,22 @@ void TestBlockLandApplication::initWorldBlocksLight() {
 }
 
 void TestBlockLandApplication::createChunk(const int StartX, const int StartY, const int StartZ) {
-	block_t LastBlock = 0;
+	Block LastBlock = 0;
 
-	Ogre::ManualObject* MeshChunk = new Ogre::ManualObject("MeshMatChunk" + Ogre::StringConverter::toString(m_ChunkID));
+	Ogre::ManualObject* MeshChunk = new Ogre::ManualObject("MeshMatChunk" + Ogre::StringConverter::toString(_chunkID));
 
 	int iVertex = 0;
-	block_t Block;
-	block_t Block1;
+	Block Block;
+	Block Block1;
 
 	/* Only create visible faces of chunk */
-	block_t DefaultBlock = 1;
+	Block DefaultBlock = 1;
 	const int SX = 0;
 	const int SY = 0;
 	const int SZ = 0;
 
 	for (int iBlock = 1; iBlock <= 4; ++iBlock) {
-		MeshChunk->begin(g_BlockInfo[iBlock].Name);
+		MeshChunk->begin(BLOCKINFO[iBlock].name);
 		iVertex = 0;
 
 		for (int z = StartZ; z < CHUNK_SIZE + StartZ; ++z) {
@@ -190,7 +190,7 @@ void TestBlockLandApplication::createChunk(const int StartX, const int StartY, c
 
 					//x+1
 					Block1 = DefaultBlock;
-					if (x < SX + WORLD_WIDTH - 1)
+					if (x < SX + _worldXSize - 1)
 						Block1 = GetBlock(x + 1, y, z);
 
 					if (Block1 == 0) {
@@ -240,7 +240,7 @@ void TestBlockLandApplication::createChunk(const int StartX, const int StartY, c
 
 					//y+1
 					Block1 = DefaultBlock;
-					if (y < SY + WORLD_HEIGHT - 1)
+					if (y < SY + _worldYSize - 1)
 						Block1 = GetBlock(x, y + 1, z);
 
 					if (Block1 == 0) {
@@ -290,7 +290,7 @@ void TestBlockLandApplication::createChunk(const int StartX, const int StartY, c
 
 					//z+1
 					Block1 = DefaultBlock;
-					if (z < SZ + WORLD_SIZE - 1)
+					if (z < SZ + _worldHeight - 1)
 						Block1 = GetBlock(x, y, z + 1);
 
 					if (Block1 == 0) {
@@ -320,46 +320,50 @@ void TestBlockLandApplication::createChunk(const int StartX, const int StartY, c
 
 	mSceneMgr->getRootSceneNode()->createChildSceneNode()->attachObject(MeshChunk);
 
-	m_pBlockChunks[StartX / CHUNK_SIZE][StartY / CHUNK_SIZE][StartZ / CHUNK_SIZE] = MeshChunk;
-	m_BlockVertexCount[StartX / CHUNK_SIZE][StartY / CHUNK_SIZE][StartZ / CHUNK_SIZE] = iVertex;
+	_blockChunkObjects[StartX / CHUNK_SIZE][StartY / CHUNK_SIZE][StartZ / CHUNK_SIZE] = MeshChunk;
+	_blockVertexCount[StartX / CHUNK_SIZE][StartY / CHUNK_SIZE][StartZ / CHUNK_SIZE] = iVertex;
 
-	++m_ChunkID;
+	++_chunkID;
 }
 
 bool TestBlockLandApplication::keyPressed(const OIS::KeyEvent &arg) {
 	if (arg.key == OIS::KC_K) {
-		m_WorldTime += 0.1f;   //Increase world time a bit
-		if (m_WorldTime > 24.0f)
-			m_WorldTime -= 24.0f;
+		_worldTime += 0.1f;   //Increase world time a bit
+		if (_worldTime > 24.0f)
+			_worldTime -= 24.0f;
 
-		ComputeWorldLightValues(m_WorldTime);
-		UpdateSceneLighting();
+		computeWorldLightValues(_worldTime);
+		updateSceneLighting();
 
-		++m_UpdateChunksCount;
+		++_updateChunksCount;
 	} else if (arg.key == OIS::KC_J) {
-		m_WorldTime -= 0.1f; //Decrease world time a bit
-		if (m_WorldTime < 0)
-			m_WorldTime += 24.0f;
+		_worldTime -= 0.1f; //Decrease world time a bit
+		if (_worldTime < 0)
+			_worldTime += 24.0f;
 
-		ComputeWorldLightValues(m_WorldTime);
-		UpdateSceneLighting();
+		computeWorldLightValues(_worldTime);
+		updateSceneLighting();
 
-		++m_UpdateChunksCount;
+		++_updateChunksCount;
 	}
 
 	return BaseApplication::keyPressed(arg);
 }
 
+void TestBlockLandApplication::updateSceneLighting() {
+
+}
+
 bool TestBlockLandApplication::frameEnded(const Ogre::FrameEvent &evt) {
 	//This makes the world day last about 5 minutes of real time
-	m_WorldTime += evt.timeSinceLastFrame * 0.04;
-	m_WorldTime = fmod(m_WorldTime, 24.0f);
+	_worldTime += evt.timeSinceLastFrame * 0.04;
+	_worldTime = fmod(_worldTime, 24.0f);
 
-	ComputeWorldLightValues(m_WorldTime);
-	UpdateSceneLighting();
+	computeWorldLightValues(_worldTime);
+	updateSceneLighting();
 
-	if (m_UpdateChunksCount == 0)
-		++m_UpdateChunksCount;
+	if (_updateChunksCount == 0)
+		++_updateChunksCount;
 
 	//Update a few chunks of the world each frame
 	updateChunksFrame();
@@ -367,45 +371,45 @@ bool TestBlockLandApplication::frameEnded(const Ogre::FrameEvent &evt) {
 }
 
 void TestBlockLandApplication::updateChunksFrame() {
-	int NUM_CHUNKS = WORLD_SIZE / CHUNK_SIZE;
-	if (m_UpdateChunksCount <= 0)
+	int NUM_CHUNKS = _worldHeight / CHUNK_SIZE;
+	if (_updateChunksCount <= 0)
 		return;
 
 	for (int i = 0; i < NUM_UPDATE_CHUNKS; ++i) {
 		// Ignore chunks with nothing in it
-		if (m_BlockVertexCount[m_UpdateChunkX][m_UpdateChunkY][m_UpdateChunkZ] > 0) {
+		if (_blockVertexCount[_updateChunkX][_updateChunkY][_updateChunkZ] > 0) {
 			// Remove and delete the existing mesh from the scene
-			if (m_pBlockChunks[m_UpdateChunkX][m_UpdateChunkY][m_UpdateChunkZ] != nullptr) {
-				Ogre::ManualObject* pMeshChunk = m_pBlockChunks[m_UpdateChunkX][m_UpdateChunkY][m_UpdateChunkZ];
+			if (_blockChunkObjects[_updateChunkX][_updateChunkY][_updateChunkZ] != nullptr) {
+				Ogre::ManualObject* pMeshChunk = _blockChunkObjects[_updateChunkX][_updateChunkY][_updateChunkZ];
 				pMeshChunk->detachFromParent();
 				delete pMeshChunk;
-				m_pBlockChunks[m_UpdateChunkX][m_UpdateChunkY][m_UpdateChunkZ] = nullptr;
+				_blockChunkObjects[_updateChunkX][_updateChunkY][_updateChunkZ] = nullptr;
 			}
 
 			// Create a new mesh for the chunk
-			Ogre::ManualObject* MeshChunk = new Ogre::ManualObject("MeshMatChunk" + Ogre::StringConverter::toString(m_ChunkID));
+			Ogre::ManualObject* MeshChunk = new Ogre::ManualObject("MeshMatChunk" + Ogre::StringConverter::toString(_chunkID));
 			MeshChunk->setDynamic(true);
 			MeshChunk->begin("Combine4");
-			createChunkCombineMat(MeshChunk, m_UpdateChunkX * CHUNK_SIZE, m_UpdateChunkY * CHUNK_SIZE, m_UpdateChunkZ * CHUNK_SIZE);
-			m_pBlockChunks[m_UpdateChunkX][m_UpdateChunkY][m_UpdateChunkZ] = MeshChunk;
+			createChunkCombineMat(MeshChunk, _updateChunkX * CHUNK_SIZE, _updateChunkY * CHUNK_SIZE, _updateChunkZ * CHUNK_SIZE);
+			_blockChunkObjects[_updateChunkX][_updateChunkY][_updateChunkZ] = MeshChunk;
 			mSceneMgr->getRootSceneNode()->createChildSceneNode()->attachObject(MeshChunk);
-			++m_ChunkID;
+			++_chunkID;
 		}
 
 		// Find the next chunk to update
-		++m_UpdateChunkX;
+		++_updateChunkX;
 
-		if (m_UpdateChunkX >= NUM_CHUNKS) {
-			m_UpdateChunkX = 0;
-			++m_UpdateChunkZ;
+		if (_updateChunkX >= NUM_CHUNKS) {
+			_updateChunkX = 0;
+			++_updateChunkZ;
 
-			if (m_UpdateChunkZ >= NUM_CHUNKS) {
-				m_UpdateChunkZ = 0;
-				++m_UpdateChunkY;
+			if (_updateChunkZ >= NUM_CHUNKS) {
+				_updateChunkZ = 0;
+				++_updateChunkY;
 
-				if (m_UpdateChunkY >= NUM_CHUNKS) {
-					m_UpdateChunkY = 0;
-					--m_UpdateChunksCount;
+				if (_updateChunkY >= NUM_CHUNKS) {
+					_updateChunkY = 0;
+					--_updateChunksCount;
 				}
 			}
 		}
@@ -437,9 +441,9 @@ Ogre::MaterialPtr TestBlockLandApplication::createTexture(const Ogre::String& pN
 }
 
 void TestBlockLandApplication::createWorldChunks() {
-	for (int z = 0; z < WORLD_SIZE; z += CHUNK_SIZE) {
-		for (int y = 0; y < WORLD_HEIGHT; y += CHUNK_SIZE) {
-			for (int x = 0; x < WORLD_WIDTH; x += CHUNK_SIZE) {
+	for (int z = 0; z < _worldHeight; z += CHUNK_SIZE) {
+		for (int y = 0; y < _worldYSize; y += CHUNK_SIZE) {
+			for (int x = 0; x < _worldXSize; x += CHUNK_SIZE) {
 				createChunk(x, y, z);
 			}
 		}
@@ -462,7 +466,7 @@ void TestBlockLandApplication::createScene() {
 	createWorldChunks();
 }
 
-void TestBlockLandApplication::ComputeWorldLightValues(const float WorldTime) {
+void TestBlockLandApplication::computeWorldLightValues(const float WorldTime) {
 	//6am = SunRise
 	//Light is symmetric about noon
 	//4am-8am = dawn
@@ -472,56 +476,56 @@ void TestBlockLandApplication::ComputeWorldLightValues(const float WorldTime) {
 
 	float BaseWorldTime = std::fmod(WorldTime, 24);
 
-	m_LightAngle = BaseWorldTime / 24.0f * 360.0f - 90.0f;
-	if (m_LightAngle < 0)
-		m_LightAngle += 360.0f;
+	_lightAngle = BaseWorldTime / 24.0f * 360.0f - 90.0f;
+	if (_lightAngle < 0)
+		_lightAngle += 360.0f;
 
 	if (BaseWorldTime <= 4 || BaseWorldTime >= 20) {
-		m_LightColor = Ogre::ColourValue(0.1f, 0.1f, 0.1f);
+		_lightColor = Ogre::ColourValue(0.1f, 0.1f, 0.1f);
 	} else if (BaseWorldTime >= 8 && BaseWorldTime <= 16) {
-		m_LightColor = Ogre::ColourValue(1, 1, 1);
+		_lightColor = Ogre::ColourValue(1, 1, 1);
 	} else if (BaseWorldTime >= 4 && BaseWorldTime <= 6) {
-		m_LightColor.r = (BaseWorldTime - 4.0f) / 2.0f * 0.9f + 0.1f;
-		m_LightColor.g = (BaseWorldTime - 4.0f) / 2.0f * 0.5f + 0.1f;
-		m_LightColor.b = (BaseWorldTime - 4.0f) / 2.0f * -0.06f + 0.1f;
+		_lightColor.r = (BaseWorldTime - 4.0f) / 2.0f * 0.9f + 0.1f;
+		_lightColor.g = (BaseWorldTime - 4.0f) / 2.0f * 0.5f + 0.1f;
+		_lightColor.b = (BaseWorldTime - 4.0f) / 2.0f * -0.06f + 0.1f;
 	} else if (BaseWorldTime >= 6 && BaseWorldTime <= 8) {
-		m_LightColor.r = 1.0f;
-		m_LightColor.g = (BaseWorldTime - 6.0f) / 2.0f * 0.4f + 0.6f;
-		m_LightColor.b = (BaseWorldTime - 6.0f) / 2.0f * 0.96f + 0.04f;
+		_lightColor.r = 1.0f;
+		_lightColor.g = (BaseWorldTime - 6.0f) / 2.0f * 0.4f + 0.6f;
+		_lightColor.b = (BaseWorldTime - 6.0f) / 2.0f * 0.96f + 0.04f;
 	} else if (BaseWorldTime >= 16 && BaseWorldTime <= 18) {
-		m_LightColor.r = 1.0f;
-		m_LightColor.g = (18.0f - BaseWorldTime) / 2.0f * 0.4f + 0.6f;
-		m_LightColor.b = (18.0f - BaseWorldTime) / 2.0f * 0.96f + 0.04f;
+		_lightColor.r = 1.0f;
+		_lightColor.g = (18.0f - BaseWorldTime) / 2.0f * 0.4f + 0.6f;
+		_lightColor.b = (18.0f - BaseWorldTime) / 2.0f * 0.96f + 0.04f;
 	} else if (BaseWorldTime >= 18 && BaseWorldTime <= 20) {
-		m_LightColor.r = (20.0f - BaseWorldTime) / 2.0f * 0.9f + 0.1f;
-		m_LightColor.g = (20.0f - BaseWorldTime) / 2.0f * 0.5f + 0.1f;
-		m_LightColor.b = (20.0f - BaseWorldTime) / 2.0f * -0.06f + 0.1f;
+		_lightColor.r = (20.0f - BaseWorldTime) / 2.0f * 0.9f + 0.1f;
+		_lightColor.g = (20.0f - BaseWorldTime) / 2.0f * 0.5f + 0.1f;
+		_lightColor.b = (20.0f - BaseWorldTime) / 2.0f * -0.06f + 0.1f;
 	} else	//Shouldn't get here
 	{
-		m_LightColor = Ogre::ColourValue(1, 1, 1);
+		_lightColor = Ogre::ColourValue(1, 1, 1);
 	}
 
-	m_AmbientColor = m_LightColor / 3.0f;
-	m_FogColor = m_LightColor * 0.80f;
+	_ambientColor = _lightColor / 3.0f;
+	_fogColor = _lightColor * 0.80f;
 }
 
 void TestBlockLandApplication::initWorldBlocksSphere() {
-	for (int i = 1; g_BlockInfo[i].ID != 255; ++i) {
-		createSolidTexture(g_BlockInfo[i].Name, g_BlockInfo[i].Color);
+	for (BlockType i = BlockType::Air; BLOCKINFO[i].type != BlockType::Max; ++i) {
+		createSolidTexture(BLOCKINFO[i].name, BLOCKINFO[i].color);
 	}
 
 	Ogre::Image heightMap;
 	heightMap.load("heightmap.png", Ogre::ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME);
 	const Ogre::PixelBox& pb = heightMap.getPixelBox();
-	WORLD_WIDTH = pb.getWidth();
-	WORLD_HEIGHT = pb.getHeight();
-	m_Blocks = new block_t[WORLD_HEIGHT * WORLD_WIDTH * WORLD_SIZE];
-	memset(m_Blocks, 0, sizeof(block_t) * WORLD_HEIGHT * WORLD_WIDTH * WORLD_SIZE);
+	_worldXSize = pb.getWidth();
+	_worldYSize = pb.getHeight();
+	_blocks = new Block[_worldYSize * _worldXSize * _worldHeight];
+	memset(_blocks, 0, sizeof(Block) * _worldYSize * _worldXSize * _worldHeight);
 
-	for (int y = 0; y < WORLD_HEIGHT; ++y) {
-		for (int x = 0; x < WORLD_WIDTH; ++x) {
+	for (int y = 0; y < _worldYSize; ++y) {
+		for (int x = 0; x < _worldXSize; ++x) {
 			const Ogre::ColourValue& color = heightMap.getColourAt(x, y, 0);
-			const int height = static_cast<int>((((color.r + color.g + color.b) / 1.5f) - 1.0f) * WORLD_SIZE / 4.0f + WORLD_SIZE / 2.0f);
+			const int height = static_cast<int>((((color.r + color.g + color.b) / 1.5f) - 1.0f) * _worldHeight / 4.0f + _worldHeight / 2.0f);
 			for (int z = 0; z < height; ++z) {
 				GetBlock(x, y, z) = (rand() % 4) + 1;
 			}
--- src/voxel/src/TestBlockLandApplication.h
+++ src/voxel/src/TestBlockLandApplication.h
@@ -2,53 +2,63 @@
 
 #include "BaseApplication.h"
 
-typedef unsigned char block_t;
-typedef unsigned char blocklight_t;
+enum class BlockType {
+	Air, Grass, Soil, Rock, Max
+};
 
-struct blockinfo_t {
-	block_t ID;
-	char Name[64];
-	Ogre::ColourValue Color;
+typedef unsigned char LightValue;
+
+struct Block {
+	BlockType type;
+	LightValue light;
+};
+
+struct BlockInfo {
+	BlockType type;
+	const char* name;
+	Ogre::ColourValue color;
 };
 
 class TestBlockLandApplication: public BaseApplication {
 private:
-	static const int WORLD_SIZE = 256;
-	int WORLD_WIDTH;
-	int WORLD_HEIGHT;
+	static const int NUM_UPDATE_CHUNKS = 16;
 	static const int CHUNK_SIZE = 16;
+	static const int _worldHeight = 256;
+	int _worldXSize;
+	int _worldYSize;
 
-	int m_ChunkID;		        // Used for uniquely naming our chunks
+	// Used for uniquely naming our chunks
+	int _chunkID;
 
-	block_t* m_Blocks;	        // Holds the block worlds in a [WORLD_SIZE][WORLD_SIZE][WORLD_SIZE] array
+	Block* _blocks;
 
-	Ogre::ColourValue m_LightColor;
-	Ogre::ColourValue m_AmbientColor;
-	Ogre::ColourValue m_FogColor;
-	float m_LightAngle;
+	Ogre::ColourValue _lightColor;
+	Ogre::ColourValue _ambientColor;
+	Ogre::ColourValue _fogColor;
+	float _lightAngle;
 
-	float m_WorldTime;
-	int m_UpdateChunkX;              //The current chunk index we're updating
-	int m_UpdateChunkY;
-	int m_UpdateChunkZ;
-	int m_UpdateChunksCount;        //Keeps track of whether to update chunks or not
-	static const int NUM_UPDATE_CHUNKS = 16;     //Number of chunks to update each frame
+	float _worldTime;
+	// The current chunk index we're updating
+	int _updateChunkX;
+	int _updateChunkY;
+	int _updateChunkZ;
+	// Keeps track of whether to update chunks or not
+	int _updateChunksCount;
+	// Number of chunks to update each frame
 
-	//Stores the mesh object for each chunk
-	Ogre::ManualObject** m_pBlockChunks;
+	// Stores the mesh object for each chunk
+	Ogre::ManualObject** _blockChunkObjects;
 
-	//The number of vertices in each mesh chunk
-	int *m_BlockVertexCount;
+	// The number of vertices in each mesh chunk
+	int *_blockVertexCount;
 
 	// Read/write access method for our block world (doesn't check input)
-	inline block_t& GetBlock(const int x, const int y, const int z) const {
-		return m_Blocks[x + y * WORLD_WIDTH + z * WORLD_HEIGHT * WORLD_WIDTH];
+	inline Block& getBlock(const int x, const int y, const int z) const {
+		return _blocks[x + y * _worldXSize + z * _worldYSize * _worldXSize];
 	}
 
-	blocklight_t* m_BlockLight;
-
-	block_t& GetBlockLight(const int x, const int y, const int z) {
-		return m_BlockLight[x + y * WORLD_WIDTH + z * WORLD_HEIGHT * WORLD_WIDTH];
+	inline LightValue& getBlockLight(const int x, const int y, const int z) const {
+		return getBlock(x, y, z).light;
 	}
 
 	void initWorldBlocksLight();
@@ -56,12 +66,14 @@ private:
 	void createWorldChunks();
 	void updateChunksFrame();
 
+	void updateSceneLighting();
+
 	// Used for filling our block world
 	void initWorldBlocksSphere();
 
 	bool frameEnded(const Ogre::FrameEvent &evt) override;
 	bool keyPressed(const OIS::KeyEvent &arg) override;
-	void ComputeWorldLightValues(const float WorldTime);
+	void computeWorldLightValues(const float WorldTime);
 	Ogre::MaterialPtr createTexture(const Ogre::String& pName, const Ogre::String& pImageFilename);
 	Ogre::MaterialPtr createSolidTexture(const Ogre::String& pName, const Ogre::ColourValue& color);
 public:
